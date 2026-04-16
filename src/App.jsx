c58140b6228dd13ac04f8e0094699afd184a468e
dheaaisyah <dheaaisyahputri09@gmail.com>
2026-04-16 15:42:54 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Dashboard from "./pages/dashboard"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
   return <Dashboard />
